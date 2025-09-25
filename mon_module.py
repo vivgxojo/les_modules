9ee13608c8ac24693b7e0c6e1d375cf2f45237e1
vivgxojo <maryse.mongeau@cegepoutaouais.qc.ca>
2025-09-25 10:46:35 -0400

    Les modules

---
+++ mon_module.py
@@ -0,0 +1,27 @@
+def ajouter_potion(inventaire: list[str], potion: str) -> None:
+    """
+    Fonction pour ajouter une potion dans l'inventaire
+    :param inventaire: Liste de potions
+    :param potion: Nouvelle potion
+    """
+    inventaire.append(potion)
+    print(f"La potion {potion} a été ajoutée dans l'inventaire.")
+
+def copier_inventaire(inventaire: list[str]) -> list[str]:
+    """
+    Fonction pour copier dans l'inventaire
+    :param inventaire: Liste de potions
+    :return: Une copie de la liste de potions
+    """
+    copie = inventaire.copy()
+    return copie
+
+def additionner_potions(total: int, nb: int) -> int:
+    """
+    Fonction pour additionner les potions de l'inventaire
+    :param total: Total précédent
+    :param nb: nb à ajouter
+    :return: nouveau total
+    """
+    total += nb
+    return total
---
+++ mon_programme.py
@@ -0,0 +1,22 @@
+from mon_module import *
+
+mon_inventaire = []
+total_inventaire = 0
+total_copie = 0
+total_autre_copie = 0
+
+ajouter_potion(mon_inventaire, "Invisibilité")
+total_inventaire = additionner_potions(total_inventaire, 1)
+
+ma_copie = copier_inventaire(mon_inventaire)
+total_copie = total_inventaire
+mon_autre_copie = mon_inventaire # ça reste la même liste, pas une copie
+total_autre_copie = total_inventaire # vrai copie de la variable
+
+ajouter_potion(mon_inventaire, "Guérison")
+total_inventaire = additionner_potions(total_inventaire, 1)
+
+
+print("Inventaire:", mon_inventaire, "Total:", total_inventaire)
+print("Ma copie:", ma_copie, "Total:", total_copie)
+print("Autre copie:", mon_autre_copie, "Total:", total_autre_copie)
