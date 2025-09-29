df3a8ee2052907cdffa1c946188808afa6211544
RCJuniorP <2541215@cegepoutaouais.qc.ca>
2025-09-29 09:16:16 -0400

    Ajout d'une autre potion

--- mon_programme.py
+++ mon_programme.py
@@ -16,6 +16,8 @@ total_autre_copie = total_inventaire # vrai copie de la variable
 ajouter_potion(mon_inventaire, "Guérison")
 total_inventaire = additionner_potions(total_inventaire, 1)
 
+ajouter_potion(mon_inventaire, "Vitesse")
+total_inventaire = additionner_potions(total_inventaire, 1)
 
 print("Inventaire:", mon_inventaire, "Total:", total_inventaire)
 print("Ma copie:", ma_copie, "Total:", total_copie)
